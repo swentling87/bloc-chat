9056072dc95d882f39a10045a21f13ad5f977f5b
Stephen Wentling <swentling87@gmail.com>
2016-08-18 22:38:07 -0700

    cp_4 complete, cp_6 partial

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -1,15 +1,22 @@
 (function() {
   function Room($firebaseArray) {
     var ref = firebase.database().ref().child("rooms");
+    var mesRef = firebase.database().ref().child("messages").orderByChild('roomId');
     var rooms = $firebaseArray(ref);
+    var messages = $firebaseArray(mesRef);
 
     var addChatRoom = function(newRoom) {
       rooms.$add({name: newRoom});
     };
 
+    var findMessages = function(roomId) {
+      return $firebaseArray(mesRef.equalTo(roomId));
+    };
+
     return {
       all: rooms,
-      createRoom: addChatRoom
+      createRoom: addChatRoom,
+      messageFinder: findMessages
     };
   }
 
